7266186ed78c98652a3433109275ea17ebe413c5
David <davidle.97@hotmail.com>
2020-01-19 16:40:26 +1100

    Chamged the default sample size to 54 and fixed the latlng co-ordinate input field

--- js/map.js
+++ js/map.js
@@ -9,7 +9,6 @@ var deleteMenu;
 
 function initialize() {
     // google.charts.load('current', {'packages':['corechart', 'line']});
-    localStorage.setItem('step', 256);
 
     map = new google.maps.Map(document.getElementById('map'), {
         zoom: 10,
@@ -23,6 +22,11 @@ function initialize() {
         strokeColor: '#0000CC',
         strokeOpacity: 0.4,
         strokeWeight: 2,
+        icons: [{
+            icon: {path: google.maps.SymbolPath.FORWARD_OPEN_ARROW},
+            offset: '50%',
+            repeat: '20%'
+        }], 
         map: map
     });
 
@@ -59,6 +63,7 @@ function addLatLng(event) {
     // Because path is an MVCArray, we can simply append a new coordinate
     // and it will automatically appear.
     if (!deleteMenu.isOpen) {
+        console.log(event.latLng);
         path.push(event.latLng);
         poly.setPath(path);
     } else {
@@ -71,26 +76,10 @@ function addLatLng(event) {
 
 function latLngInput() {
     var val = document.getElementById("latlng").value;
-    console.log(val)
-    console.log(val.split(",").length == 2)
-    console.log(parseFloat(val.split(",")[1]))
-    if(val.split(",").length == 2 & parseFloat(val.split(",")[1])) {
-        val = {lat: parseFloat(val.split(",")[0]), lng: parseFloat(val.split(",")[1])};
-        console.log(val);
-
-        var path = poly.getPath();
-        console.log(path);
-
-        // Because path is an MVCArray, we can simply append a new coordinate
-        // and it will automatically appear.
-        if (!deleteMenu.isOpen) {
-            path.push(val);
-            poly.setPath(path);
-        } else {
-            deleteMenu.isOpen = false;
-        }
+    if(val.split(",").length == 2 & !isNaN(parseFloat(val.split(",")[1]))) {
+        val = new google.maps.LatLng({lat: parseFloat(val.split(",")[0]), lng: parseFloat(val.split(",")[1])});
+        addLatLng({latLng: val})
     }
-    
 }
 
 google.maps.event.addDomListener(window, 'load', initialize);
